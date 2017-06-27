238ecdc70fd1b9a5700bc7e3bf4e88f074ce71f0
Doruk Kutlu <dorukk@gmail.com>
2017-06-28 00:56:32 +0300

    up to babel-loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-require("./index.css");
-console.dir(navigator);
+import "./index.css"
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Component = () => <h1>Merhaba Dünya</h1>
+
+ReactDOM.render(<Component />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = function(env) {
   return {
@@ -8,25 +11,42 @@ module.exports = function(env) {
       "main": "./index.js",
     },
     output: {
-      filename: "bundle.js",
+      filename: "[name]_[hash:5].js",
       path: path.resolve(__dirname, "build"),
+      // publicPath: "//cdn.example.com/"
     },
     module: {
       rules: [
         {
           test: /\.css$/,
           include: path.resolve(__dirname, "src"),
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: [
+              { loader: "css-loader" },
+            ]
+          })
+        },
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, "src"),
           use: [
             {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader"
+              loader: "babel-loader",
+              options: { presets: ["react", "env"] }
             }
           ]
         }
       ]
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Taytıl",
+        template: "template.html"
+      }),
+      new webpack.WatchIgnorePlugin([path.resolve(__dirname, "build")]/*, { watch: true }*/),
+      new ExtractTextPlugin({ filename: "[name]_[hash:5].css" }),
+      new CleanWebpackPlugin([path.resolve(__dirname, "build")]),
+    ],
   }
 }
